50b6f3b092270a270c775b722366702ff1dbc2ab
Karol Binkowski <karolbinkowski3@protonmail.com>
2022-06-15 23:11:11 +0200

    add bulma design

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 900,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
